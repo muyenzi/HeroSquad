eb19762f84423d94b91ca8df4a622ee965a1d951
Muyenzi Raissa <muyenziraissa@gmail.com>
2019-09-24 12:58:39 +0200

    removing the test annotation

--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -1,8 +1,5 @@
 
 
-//public class HeroTest {
-//    @Test
-//    public void Hero_instantiatesCorrectly_true() {
 //        Hero myHero = new Hero("Omni-man", "24", "Ever Present", "Easily Overwhelmed");
 //        assertEquals(true, myHero instanceof Hero);
 //    }
