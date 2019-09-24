457355cb1ce21083e16e5a8aad0668c78b888fd2
Muyenzi Raissa <muyenziraissa@gmail.com>
2019-09-24 12:55:20 +0200

    the imports part of the test

--- src/test/java/models/SquadTest.java
+++ src/test/java/models/SquadTest.java
@@ -1,8 +1,6 @@
 
 
-//public class SquadTest {
-//    @Test
-//    public void getPublished_isFalseAfterInstantiation_false() throws Exception {
+//
 //        Squad mySquad = new Squad("Day 1: Intro", "club");
 //        assertEquals(false, mySquad.getHeroes()); //should never start as published
 //    }
